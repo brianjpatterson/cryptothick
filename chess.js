d1561f9ef93ec40c571c5143eefdbc0c516f75f5
cryptothick <38083399+cryptothick@users.noreply.github.com>
2018-04-04 14:01:39 -0700

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,25 +1,32 @@
 (function ($) {
     $(function () {  //document.ready
         
-       // var gameUI = initUI();
+       var gameUI = initUI();
         
        
-}}
     });
     
     function initUI() {
     
        var ui = {};
+       ui.gameBoard = drawBoard($("#gameBoard"));
+       
        return ui;
     
     };
     
     function drawBoard(e) {
-        var table = $('<table>').addClass('foo');
-for(i=0; i<3; i++){
-    var row = $('<tr>').addClass('bar').text('result ' + i);
-    table.append(row);
-}
+      
+        for(i=0; i<8; i++){
+            var row = $("<tr>");
+            for(j=0;j<8;j++){
+                var cls = j%2===0 ? "black" : "white";
+                var cell = $('<td>').addClass(cls);
+                row.append(cell);
+            }
+            e.append(row);
+        }
+        return e;
     };
     
 })(jQuery);
