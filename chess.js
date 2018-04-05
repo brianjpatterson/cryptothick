531a510e80bda45a14f3f1d93d3dcd163da7bda1
cryptothick <38083399+cryptothick@users.noreply.github.com>
2018-04-04 17:41:29 -0700

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,50 +1,56 @@
 (function ($) {
     $(function () {  //document.ready
         
-       var gameUI = initUI();
-        
-       $("content").hide();
+       var ui = new UserInterface($("#gameBoard"));
+       
     });
     
-    function initUI() {
+    function UserInterface(e) {
     
-       var ui = {};
-       ui.gameData = initData();
-       ui.gameBoard = drawBoard($("#gameBoard"));
+       this.Game = function(e) {
+           
+           var self = this;
+           
+           this.data = {
+               "cols" : ["a","b","c","d","e","f"."g","h"],
+               "rows" : [1,2,3,4,5,6,7,8]
+           }
+       
+           this.board = this.drawBoard(e);
+           
+           this.drawBoard = function(e) {
+               
+               var offset = false;
+               for(i=0; i<8; i++){
+                   
+                   var row = $("<tr>");
+                   offset = offset === true ? false : true;
+                   
+                   for(j=0;j<8;j++){
+                       
+                       var cls = j%2===0 ? 
+                           (offset ? "black" : "white"):
+                           (offset ? "white" : "black");
+                       var cell = $('<td>').addClass(cls);
+                       var id = self.data.cols[i] + self.data.rows[j].toString();
+                       
+                       cell.attr("id",id);
+                       row.append(cell);
+                   }
+                   e.append(row);
+               }
+               return e;
+           }
+       }
+       
+       this.game = new this.Game(e);
        
        return ui;
     
     };
     
-    function initData() {
-        
-        var dat = {};
-        
-        dat.cols = ["a","b","c","d","e","f","g","h"];
-        dat.rows = [1,2,3,4,5,6,7,8];
-        
-        return dat;
-    }
-    
-    function drawBoard(e) {
-        
-        var offset = false;
-        for(i=0; i<8; i++){
-            var row = $("<tr>");
-            offset = offset === true ? false : true;
-            for(j=0;j<8;j++){
-                var cls = j%2===0 ? 
-                           (offset ? "black" : "white"):
-                           (offset ? "white" : "black");
-                var cell = $('<td>').addClass(cls);
-                var id = ui.gameData.cols[j] + ui.gameData.rows[i].toString;
-                cell.attr("id",id);
-                row.append(cell);
-            }
-            e.append(row);
-        }
-        return e;
-    };
+  
+  
     
 })(jQuery);
     
