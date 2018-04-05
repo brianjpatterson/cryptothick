3b6bb8ec9938572cd2f8067e803dac67a8a3537d
cryptothick <38083399+cryptothick@users.noreply.github.com>
2018-04-04 21:59:10 -0700

    Update chess.js

--- chess.js
+++ chess.js
@@ -14,28 +14,10 @@
                UserInterface.o = this;
            }
            this._e = e;
-           this._d = new Data();
-           this.game = new Game();
-           this.board = this.game.drawboard(this._e, this._d);
+           this.cols = ["a","b","c","d","e","f"."g","h"];
+           this.rows = ["1","2","3","4","5","6","7","8"]; 
            
-       } else {
-           obj = new UserInterface();
-           ret = UserInterface.apply(obj, arguments);
-           return ret === undefined ? obj : ret;
-       }
-       
-       return UserInterface.o;
-    };
-    
-    function Data() {
-       
-        this.cols = ["a","b","c","d","e","f"."g","h"];
-        this.rows = ["1","2","3","4","5","6","7","8"];  
-    };
-    
-    function Game() {
-           
-           this.drawBoard = function(e,d) {
+           this.board = function() {
                alert("gfgh");
               
                var offset = false;
@@ -50,23 +32,26 @@
                            (offset ? "black" : "white"):
                            (offset ? "white" : "black");
                        var cell = $('<td>').addClass(cls);
-                       var id = d.cols[i] + d.rows[j];
+                       var id = this.cols[i] + this.rows[j];
                       
                        cell.attr("id",id);
                        row.append(cell);
                    }
-                   e.append(row);
+                   this._e.append(row);
                }
-               return e;
+               return this._e;
            }
+           
+       } else {
+           obj = new UserInterface();
+           ret = UserInterface.apply(obj, arguments);
+           return ret === undefined ? obj : ret;
+       }
        
-       
-       
-       
-       
-    
+       return UserInterface.o;
     };
     
+  
   
   
     
