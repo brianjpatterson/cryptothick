f4951cee8c402a4fe6e405959c0d755ddf9d9a5b
cryptothick <38083399+cryptothick@users.noreply.github.com>
2018-04-04 21:49:47 -0700

    Update chess.js

--- chess.js
+++ chess.js
@@ -6,30 +6,38 @@
     });
     
     function UserInterface(e) {
-    
-       
-           this.game = new Game(e);
-        
-        return this;
-           
-           };
-    
-    function Game(e) {
-    
-        
-        
-           var parent = this;
-    
-           this.data = {
-               "cols" : ["a","b","c","d","e","f"."g","h"],
-               "rows" : [1,2,3,4,5,6,7,8]
+   
+       var obj, ret;
+
+       if (this instanceof UserInterface) {
+           if(undefined == UserInterface.o) {
+               UserInterface.o = this;
            }
-       
-           this.board = this.drawBoard(e);
+           this._e = e;
+           this.data = new Data();
+           this.game = new Game();
+           this.board = this.game.drawboard(this._e, this.data);
            
-           this.drawBoard = function(e) {
+       } else {
+           obj = new UserInterface();
+           ret = UserInterface.apply(obj, arguments);
+           return ret === undefined ? obj : ret;
+       }
+       
+       return UserInterface.o;
+    };
+    
+    function Data() {
+       
+        this.cols = ["a","b","c","d","e","f"."g","h"];
+        this.rows = ["1","2","3","4","5","6","7","8"];  
+    };
+    
+    function Game() {
+           
+           this.drawBoard = function(e,d) {
                
-               this.parent = parent;
+              
                var offset = false;
                for(i=0; i<8; i++){
                    
@@ -42,8 +50,8 @@
                            (offset ? "black" : "white"):
                            (offset ? "white" : "black");
                        var cell = $('<td>').addClass(cls);
-                       var id = this.parent.data.cols[i] + this.parent.data.rows[j].toString();
-                       alert(id+"id");
+                       var id = d.cols[i] + d.rows[j];
+                      
                        cell.attr("id",id);
                        row.append(cell);
                    }
@@ -55,7 +63,7 @@
        
        
        
-       return this;
+       
     
     };
     
