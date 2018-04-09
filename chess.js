50d2a7780560e6f678910600e97a3c1a88133aeb
cryptothick <38083399+cryptothick@users.noreply.github.com>
2018-04-09 03:12:52 -0700

    Update chess.js

--- chess.js
+++ chess.js
@@ -12,7 +12,7 @@
        
        
        
-         this.c = ["a","b","c","d","e","f"."g","h"];
+        var this.c = ["a","b","c","d","e","f"."g","h"];
            //this.rows = ["1","2","3","4","5","6","7","8"]; 
            
            this.board = function(e) {
